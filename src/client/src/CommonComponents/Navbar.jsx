fa00b32e75b653c6e7def51b0fb5f4034f5574c4
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2022-01-01 20:53:56 +0530

    Add redirect on navbar

--- src/client/src/CommonComponents/Navbar.jsx
+++ src/client/src/CommonComponents/Navbar.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import {AppBar,Box,Toolbar,Typography} from '@material-ui/core';
+import {AppBar,Box, Toolbar,Typography} from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUserCheck} from '@fortawesome/free-solid-svg-icons';
+import {Link} from "react-router-dom"
 // import {Me} from '@material-ui/icons';
 // import MenuIcon from '@mui/icons-material/Menu';
 
@@ -11,9 +12,11 @@ export default function Navbar() {
       <AppBar position="static" style={{backgroundColor:'#242424'}}>
         <Toolbar>
         <FontAwesomeIcon icon={faUserCheck} />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          &nbsp;&nbsp;BCTIdentity
-          </Typography>
+          <Link to="/" style={{textDecoration: "none", color: "white"}}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            &nbsp;&nbsp;BCTIdentity
+            </Typography>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
